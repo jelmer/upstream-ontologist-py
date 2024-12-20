6fb23d1fa7af73db054065ec350800a8f51ddc47
Jelmer Vernooĳ <jelmer@jelmer.uk>
2024-12-20 12:52:46 +0000

    Release 0.2.2

--- py/upstream_ontologist/__init__.py
+++ py/upstream_ontologist/__init__.py
@@ -63,7 +63,7 @@ from . import _upstream_ontologist
 
 SUPPORTED_CERTAINTIES = ["certain", "confident", "likely", "possible", None]
 
-version_string = "0.2.1"
+version_string = "0.2.2"
 
 USER_AGENT = "upstream-ontologist/" + version_string
 # Too aggressive?
